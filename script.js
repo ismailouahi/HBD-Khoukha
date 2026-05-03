86d75e97399ea9dca8413820f978d3c30a1760b4
ismailouahi <ismail.ouahi.emi@gmail.com>
2026-05-03 18:16:26 +0200

    Fix persistent below-fold issue by locking viewport and screen scrolling

--- script.js
+++ script.js
@@ -59,6 +59,12 @@ function resetViewportToTop() {
   window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
   document.documentElement.scrollTop = 0;
   document.body.scrollTop = 0;
+
+  requestAnimationFrame(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+  });
 }
 
 function unlockExperience() {
